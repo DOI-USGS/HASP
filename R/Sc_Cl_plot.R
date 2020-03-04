8b179e14d094990535a4d3e41de45a6f4a1a3aa7
Laura <ldecicco@usgs.gov>
2020-03-04 15:54:01 -0600

    Switch to ggplot2

--- R/Sc_Cl_plot.R
+++ R/Sc_Cl_plot.R
@@ -6,9 +6,10 @@
 #' @param x data frame
 #' @param title character
 #' @export
-#' @import graphics
+#' @import ggplot2
+#' @importFrom scales comma
+#' @importFrom ggpmisc stat_poly_eq
 #' @importFrom dataRetrieval readNWISpCode
-#' @importFrom stats lm
 #' @examples 
 #' 
 #' site <- "263819081585801"
@@ -18,68 +19,44 @@
 #' Sc_Cl_plot(site_data, title = "Hi")
 Sc_Cl_plot <- function(x, title){
   
+  chloride <- sp <- ..eq.label.. <- ..rr.label.. <- ".dplyr"
+  
   # Specify the plot titles using the function getParmCodeDef
   
-  
-  Cltitle <- readNWISpCode("99220")[["parameter_nm"]]
-  Sctitle <- readNWISpCode("90095")[["parameter_nm"]]
+  Cltitle <- gsub(", unfiltered", "",readNWISpCode("99220")[["parameter_nm"]])
+  Cltitle <- gsub(", water", "", Cltitle)
+  Sctitle <- gsub(", unfiltered", "",readNWISpCode("90095")[["parameter_nm"]])
+  Sctitle <- gsub(", water", "", Sctitle)
+  Sctitle <- gsub(", laboratory", "", Sctitle)
   
   Plotdata <- Sc_Cl_table(x)
   
-  if(nrow(Plotdata)>1){
-    
-    par(oma = c(4, 1, 1, 1)) # this makes a bigger area so that the legend can go below the plot  
-    par(mar = c(6,4,3,2)+0.1) # Default margin is (5,4,4,2)+0.1 this decreases the margin on the top and bottom so that the 
-    # the plot is a little taller which provides room for the longer tic lables with the comma.
-    plot(Plotdata$sp, 
-         Plotdata$chloride, 
-         type = "p", col="blue", 
-         main = title, 
-         xlab = Sctitle, 
-         ylab = Cltitle, 
-         yaxt='n',
-         xaxt='n') # yaxt='n' and xaxt='n' set the labels to not display so that you can add your own.
-    axis(1, at=axTicks(1), labels=format(axTicks(1), big.mark=','), las=1, tck=0.05) # this adds commas for thousands to the axis labels.
-    axis(2, at=axTicks(2), labels=format(axTicks(2), big.mark=','), las=0, tck=0.05) # this adds commas for thousands to the axis labels.las=0 makes the labels parrelel to the axis.
-    axis(3, labels=FALSE, tck=0.05)
-    axis(4, labels=FALSE, tck=0.05)
-    mtext("U.S. Geological Survey", adj=0, col="dark green")
-    mtext("Graph includes provisional data", side=3, adj=1,col="magenta", cex=0.8)
-    
-    # compute a linear regression line and then add it to the plot
-    
-    Rline <- lm(Plotdata$chloride ~ Plotdata$sp)
-    
-    if(!is.na(Rline$coefficients[2])){
-      abline(Rline)
-    }
-    
-    # Determine the slope intercept and R-squared of the linear regression then paste this info into text variables
-    SumRline <- summary(Rline)
-    ClvsSC_rsqr <- SumRline$r.squared
-    CLvsSC_intcp <- Rline$coefficients[1]
-    CLvsSC_slope <- Rline$coefficients[2]
-    lmresults <- as.data.frame(cbind(round(CLvsSC_intcp,1), round(CLvsSC_slope,4), round(ClvsSC_rsqr,4)))
-    names(lmresults) <- c("Intercept", "Slope", "R-squared")
-    Ftext1 <- paste0("  y=", lmresults$Slope, "x", lmresults$Intercept)
-    Ftext2 <- paste0("  r-squared=", lmresults$`R-squared`)
-    
-    # Put this information and a ledgend on the plot
-    
-    par(fig = c(0, 1, 0, 1), 
-        oma = c(0, 0, 0, 0), 
-        mar = c(0,0,0,0), 
-        new = TRUE) # this sets the legend up
-    
-    legend("bottomright",
-           c("EXPLANATION", "Linear regression", Ftext1, Ftext2),
-           lty = c(0, 1, 0, 0),
-           col = c(NA, "black", NA, NA),
-           xpd = TRUE,
-           horiz = FALSE,
-           inset = c(0, -0.22))
-    
+  if(nrow(Plotdata) == 0){
+    stop("No data to make plot")
   }
+
+  plot_out <- ggplot(data = Plotdata,
+                     aes(x = sp, y = chloride)) +
+    geom_point(color = "blue") +
+    stat_smooth(method = "lm", color = "black", 
+                formula = y ~ x , se = FALSE) +
+    theme_bw() +
+    stat_poly_eq(formula = y ~ x,
+                 aes(label = paste(..eq.label.., ..rr.label.., sep = "~~~")), 
+                 parse = TRUE) +
+    scale_y_continuous(Cltitle, 
+                       labels = scales::comma) +
+    scale_x_continuous(Sctitle, 
+                       labels = scales::comma) +
+    geom_text(inherit.aes = FALSE, data = data.frame(),
+              hjust = 0, vjust = -0.3, size = 3, color = "darkgreen",
+              aes(x = -Inf, y = Inf, label = "U.S. Geological Survey")) +
+    coord_cartesian(clip = 'off') +
+    ggtitle(title) +
+    theme(plot.title = element_text(hjust = 0.5))
+  
+  return(plot_out)
+  
   
 }
 
