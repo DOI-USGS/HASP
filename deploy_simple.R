aeb536d4a46aaee7ad39ce3ed4a683c6fffabf47
DeCicco <ldecicco@usgs.gov>
2022-06-15 08:37:39 -0500

    Was gitignoring the pkgdown stuff for some reason....

--- deploy_simple.R
+++ deploy_simple.R
@@ -2,6 +2,9 @@ library(connectapi)
 client <- connect(server = Sys.getenv("CONNECT_SERVER"),
                   api_key = Sys.getenv("CONNECT_API_KEY"))
 
+file.copy(from = "./docs/articles/logo.png", 
+          to = "./docs/reference/logo.png")
+
 rsconnect::writeManifest(appDir = "./docs")
 bundle <- bundle_dir("./docs")
 
